4d98b40fa0b726a280f4117469759d1d6bd6c8d8
cbkinase <cambeck366@gmail.com>
2024-07-01 03:26:19 -0400

    Optimized: No need to pickle data for Bloom filters

--- src/bloom_filter.py
+++ src/bloom_filter.py
@@ -43,11 +43,9 @@ TDD with probabilistic data structures... how does it work? Monte Carlo?
 
 
 import hashlib
-import pickle
 from math import ceil
 from math import exp
 from math import log
-from math import pow
 from numbers import Number
 from typing import Hashable
 from bit_array import BitArray
@@ -93,7 +91,7 @@ class BloomFilter:
     def _make_bit_array(self):
         n = self._expected_insertions
         p = self._fp_rate
-        length = ceil((n * log(p)) / log(1 / pow(2, log(2))))
+        length = ceil(-n * log(p) / log(2) ** 2)
         return BitArray(length)
 
     def _pick_hash_functions(self):
@@ -143,10 +141,10 @@ class BloomFilter:
         Returns True if the item might have been put in this BloomFilter,
         False if this is definitely not the case.
         """
-        pickled = pickle.dumps(item)
+        signature = hash(item).to_bytes(64, byteorder='big', signed=True)
 
         for hasher in self._hash_functions:
-            digest = hasher(pickled).digest()
+            digest = hasher(signature).digest()
             hash_int = int.from_bytes(digest, byteorder="little")
             bucket = hash_int % len(self._bit_array)
 
@@ -159,10 +157,10 @@ class BloomFilter:
         """
         Put an element into the BloomFilter.
         """
-        pickled = pickle.dumps(item)
+        signature = hash(item).to_bytes(64, byteorder='big', signed=True)
 
         for hasher in self._hash_functions:
-            digest = hasher(pickled).digest()
+            digest = hasher(signature).digest()
             hash_int = int.from_bytes(digest, byteorder="little")
             bucket = hash_int % len(self._bit_array)
             self._bit_array[bucket] = 1
@@ -182,3 +180,41 @@ class BloomFilter:
         Included for uniformity with other container types.
         """
         return self.may_contain(item)
+
+    def __sizeof__(self):
+        return self._bit_array.__sizeof__()
+
+
+if __name__ == "__main__":
+    from uuid import uuid4
+    from time import perf_counter
+
+    size = 7_000_000
+    uuids = [uuid4() for _ in range(size)]
+    start = perf_counter()
+
+    b = BloomFilter(size, fp_rate=0.01)
+
+    for uuid in uuids:
+        b.put(uuid)
+
+    end = perf_counter()
+
+    print(round(end - start, 3), "seconds to add", size, "uuids")
+    bf_time = end - start
+
+    start = perf_counter()
+
+    s = set()
+    for uuid in uuids:
+        s.add(uuid)
+
+    end = perf_counter()
+    s_time = end - start
+
+    print(round(bf_time / s_time, 3), "times slower than set")
+    ssize = s.__sizeof__() / 1000 / 1000
+    bsize = b.__sizeof__() / 1000 / 1000
+    print(ssize, "MB used by set")
+    print(bsize, "MB used by Bloom filter")
+    print(round(ssize / bsize, 3), "times more space efficient")
